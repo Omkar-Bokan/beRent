652fa2f8beea5bc579f7f1ca49d64af19b746d36
Shubham Sarode <shubham.sarode@shurutech.com>
2025-08-08 17:13:01 +0530

    cores for patch

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 connectToMongo();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // app.use(cors());
 app.use(require('cors')({
